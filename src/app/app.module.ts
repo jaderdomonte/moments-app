648a6e5835566d9f3794017864d6c0b386826187
santos-valadao <jaderdomonte@gmail.com>
2025-03-16 12:20:00 -0300

    feat: create moment-from

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MomentFormComponent
   ],
   imports: [
     BrowserModule,
